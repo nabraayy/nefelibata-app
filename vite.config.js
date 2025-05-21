5acca98cba875ed87e548ace8146d0c49f254638
Nayara <garciatarraganayara@gmail.com>
2025-05-21 16:09:19 +0200

    fix: move vite to dependencies and fix build

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.jsx'],
-            refresh: true,
-        }),
-        react(),
-    ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
-    },
+  plugins: [
+    laravel({
+      input: ['resources/js/app.jsx'],
+      refresh: true,
+    }),
+    react(),
+  ],
+  base: '/build/',
 });
