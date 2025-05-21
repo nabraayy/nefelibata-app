f168829240062bd9c59836545f25ce4bc91b9b2a
Nayara <garciatarraganayara@gmail.com>
2025-05-21 15:01:37 +0200

    build: incluir public/build para producción

---
+++ public/build/assets/PrimaryButton-kMZPeUwX.js
@@ -0,0 +1,5 @@
+import{j as e}from"./app-do_TNxKp.js";function r({className:n="",disabled:t,children:o,...s}){return e.jsxs("button",{...s,disabled:t,className:`relative overflow-hidden inline-flex items-center justify-center 
+                rounded-xl px-6 py-2 text-sm font-medium tracking-wide 
+                text-white transition duration-300 ease-in-out
+                focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#FF2D20] 
+                ${t?"opacity-50 cursor-not-allowed":"hover:scale-[1.02]"} `+n,children:[e.jsx("span",{className:"absolute inset-0 z-0 bg-[url('/nube4.png')] bg-cover bg-center opacity-80 transition duration-300"}),e.jsx("span",{className:"relative z-10",children:o})]})}export{r as P};
