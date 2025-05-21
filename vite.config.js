5bd18f5346475444bbf7f3197ad446e9fbe64561
Nayara <garciatarraganayara@gmail.com>
2025-05-21 16:35:24 +0200

    su puya+

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,24 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/js/app.jsx'],
-      refresh: true,
-    }),
-    react(),
-  ],
-  base: '/build/',
+    plugins: [
+        laravel({
+            input: ['resources/js/app.jsx'],
+            refresh: true,
+        }),
+        react(),
+    ],
+    base: '/build/', // 👈 esto es crucial
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.jsx',
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]',
+            },
+        },
+    },
 });
