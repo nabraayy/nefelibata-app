1056549ebe3572ea80c280675b85600287526528
Nayara <garciatarraganayara@gmail.com>
2025-05-26 15:45:01 +0200

    d

--- public/index.php
+++ public/index.php
@@ -17,4 +17,4 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->run();
+$app->handleRequest(Request::capture());
