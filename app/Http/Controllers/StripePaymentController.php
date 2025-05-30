aece7604fc90d151638cf34c539aecb85ad9d438
Nayara <garciatarraganayara@gmail.com>
2025-05-30 10:31:51 +0200

    metodos pago

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -10,16 +10,20 @@ class StripePaymentController extends Controller
 {
    public function createIntent(Request $request)
     {
-        Stripe::setApiKey(config('services.stripe.secret'));
+        $request->validate([
+        'amount' => 'required|integer|min:100', // mínimo 1€
+    ]);
 
-        $intent = PaymentIntent::create([
-            'amount' => $request->amount,
-            'currency' => 'eur',
-            'automatic_payment_methods' => ['enabled' => true],
-        ]);
+    Stripe::setApiKey(config('services.stripe.secret'));
 
-        return response()->json([
-            'clientSecret' => $intent->client_secret,
-        ]);
+    $intent = PaymentIntent::create([
+        'amount' => $request->amount,
+        'currency' => 'eur',
+        'automatic_payment_methods' => ['enabled' => true],
+    ]);
+
+    return response()->json([
+        'clientSecret' => $intent->client_secret,
+    ]);
     }
 }
